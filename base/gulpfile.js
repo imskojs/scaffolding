dbc978c3daca60a813d3ac0a076b001b4e2dc8dd
Seunghoon Ko <imskojs@gmail.com>
2015-06-25 14:53:25 +0900

    common.scss

--- base/gulpfile.js
+++ base/gulpfile.js
@@ -21,7 +21,11 @@ var paths = {
 gulp.task('default', ['sass', 'js']);
 
 gulp.task('sass', function(done) {
-  gulp.src(['./scss/ionic.app.scss', './www/state/**/*.scss'])
+  gulp.src([
+    './scss/ionic.app.scss', 
+    './scss/ionic.app.custom.scss', 
+    './www/state/**/*.scss'
+  ])
     .pipe(sass({
       errLogToConsole: true
     }))
