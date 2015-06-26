28435bfa4f7fa32dad0659b1d9f62efa409879cf
Seunghoon Ko <imskojs@gmail.com>
2015-06-26 16:07:40 +0900

    dynamic vw

--- base/www/js/directive/viewWidthDirective.js
+++ base/www/js/directive/viewWidthDirective.js
@@ -1,11 +1,19 @@
 myApp
 .directive('vw', [
-'IndexModel', '$rootScope',
-function (IndexModel, $rootScope){
+'IndexModel', '$rootScope', '$window',
+function (IndexModel, $rootScope, $window){
     return {
         link: function (scope, element, attrs){
+            
             IndexModel.vw = element.prop('offsetWidth')
-            $rootScope.vw = IndexModel.vw
+            $rootScope.vw = IndexModel.vw;
+
+            $window.addEventListener('resize', function (){
+                $rootScope.$apply(function (){
+                    IndexModel.vw = element.prop('offsetWidth')
+                    $rootScope.vw = IndexModel.vw;
+                })
+            })
         }
     }
-}])
+}]);
